01229f7ee9d182e4a02659d5b2decbaf8adc38db
Launch Code <b.ronzell@yahoo.com$ git config --global user.name Ronzellgit config --global user.email b.ronzell@yahoo.com>
2022-09-07 15:53:52 -0500

    smthg3

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Write your JavaScript code here!
 
-const { myFetch } = require("./scriptHelper");
+const { myFetch, formSubmission } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
+    const form = this.document.querySelector("form");
+    form.addEventListener("submit", (event)=>{
+        event.preventDefault();
+        let pilot = this.document.querySelector("input[name=pilotName]").value;
+        let copilot = this.document.querySelector("input[name=copilotName").value;
+        let fuelLevel = this.document.querySelector("input[name=fuelLevel").value;
+        let cargoLevel = this.document.querySelector("input[name=cargoMass").value;
+        let list = this.document.getElementById('faultyItems');
+
+        formSubmission(this.document,list,pilot,copilot,fuelLevel,cargoLevel);
+    })
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
