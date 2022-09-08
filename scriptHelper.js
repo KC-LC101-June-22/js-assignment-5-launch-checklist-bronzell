b2cac338b3c346d5f44c3b5535fac39fcef57dc8
Launch Code <b.ronzell@yahoo.com$ git config --global user.name Ronzellgit config --global user.email b.ronzell@yahoo.com>
2022-09-08 10:52:54 -0500

    smthg4

--- scriptHelper.js
+++ scriptHelper.js
@@ -12,35 +12,35 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
            <li>Star: ${star}</li>
            <li>Distance from Earth: ${distance}</li>
            <li>Number of Moons: ${moons}</li>
-       </ol> <img src='${imageUrl}'/> ` }
+       </ol> <img src='${imageUrl}'/> ` 
+    }
                function validateInput(testInput) { 
                if (testInput === "" || testInput === null || testInput === 0) { 
                return `Empty`
                 } else if ((!isNaN(Number(testInput)))) { 
-               return `Number`
+               return `Is a Number`
                  } else { 
                return 'Not a Number'
             }
         }
 
                function formSubmission (document, list, pilot, copilot, fuelLevel, cargoLevel) {
-                   //DOM elements
-                   let} pilotStatus = document.getElementById('pilotStatus');
+                   
+               let pilotStatus = document.getElementById('pilotStatus');
                let copilotStatus = document.getElementById('copilotStatus');
                let fuelStatus = document.getElementById('fuelStatus');
                let launchStatus = document.getElementById('launchStatus');
                let cargoStatus = document.getElementById('cargoStatus');
-               //check all fields are filled
+               
                if (validateInput(pilot) === `Empty`|| validateInput(copilot) === `Empty`||
                validateInput(fuelLevel) === `Empty`||validateInput(cargoLevel) === `Empty`) {alert(`All fields are required`);
                }
            
            else if (validateInput(fuelLevel) === 'Not a Number' || validateInput(cargoLevel) === 'Not a Number') {alert(`Please enter values for Fuel Level and Cargo Mass`);
-           } else if (validateInput(pilot)===`Number`||validateInput(copilot)===`Number`) {alert('Please do not enter numbers for name of pilot/co-pilot');
-             }  
-           else {
-                   
-                   pilotStatus.innerHTML = `${pilot} is ready`;
+           } else if (validateInput(pilot)===`Is a Number`||validateInput(copilot)===`Is a Number`) {alert('Please do not enter numbers for name of pilot/co-pilot');
+             } 
+
+           else { pilotStatus.innerHTML = `${pilot} is ready`;
                copilotStatus.innerHTML = `${copilot} is ready`;
                list.style.visibility = 'hidden';
            }
@@ -61,26 +61,24 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                launchStatus.style.color = `green`;
                
         }
-
+    }
         
 
-               async function myFetch() {let} planetsReturned;{
-
-               let planetsReturned;
+               async function myFetch() {let planetsReturned;
 
                planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) { 
                return response.json()
 
-             });
+               });
                return planetsReturned;
             }
-               function pickPlanet(planets) {let} idx = Math.floor(Math.random() * planets.length);
+               function pickPlanet(planets) {let idx = Math.floor(Math.random() * planets.length);
                return planets[idx];
-           
+               }
 
                module.exports.addDestinationInfo = addDestinationInfo;
                module.exports.validateInput = validateInput;
                module.exports.formSubmission = formSubmission;
                module.exports.pickPlanet = pickPlanet;
                module.exports.myFetch = myFetch;
-           
+            
